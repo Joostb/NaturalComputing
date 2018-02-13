4fcbebf8d02ccb2928717b763ad0ea36cb8ceca7
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-02-13 15:17:52 +0100

    Completed Exercise 3 and 4

--- assignment1/1+1ga.py
+++ assignment1/1+1ga.py
@@ -1,12 +1,14 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def random_sequence(n):
     return np.random.randint(2, size=n)
 
+
 def count_ones(individual):
     return np.sum(individual)
+
 
 def mutation(gene, n, p):
     """ generate a random sequence of 0's and 1's, where each bit has
@@ -16,8 +18,8 @@ def mutation(gene, n, p):
 
     return (gene + mask) % 2  # mutate the sequence
 
+
 def ga(fitness_function, n, iterations, p):
-    
     best_individual = random_sequence(n)
     best_fitness = fitness_function(best_individual)
     l = []
@@ -27,14 +29,30 @@ def ga(fitness_function, n, iterations, p):
         if fitness > best_fitness:
             best_individual = sequence
             best_fitness = fitness
-        
+
         l.append(best_fitness)
 
     return best_individual, best_fitness, l
 
 
-i,f,l = ga(count_ones, 1000, 5000, 1/1000)
+if __name__ == "__main__":
+    n = 100
+    n_iter = 1500
+    p = 1/n
 
-plt.plot(l)
-plt.show()
+    fitnesses = []
+    times_found = 0
+    for _ in range(10):
+        _, best_fitness, fitness_list = ga(count_ones, n, n_iter, p)
+        print("Found Fitness: {}".format(best_fitness))
+        fitnesses.append(fitness_list)
+        if best_fitness == n:
+            times_found += 1
 
+    for fitness in fitnesses:
+        plt.plot(fitness)
+    plt.ylabel("Best Fitness")
+    plt.xlabel("Iteration")
+    plt.show()
+
+    print("Optimum found {} times".format(times_found))
--- assignment1/mc.py
+++ assignment1/mc.py
@@ -1,36 +1,57 @@
-
-import random
-from typing import Callable
-
-def random_sequence(length : int) -> int:
-    """ An integer is really a string of bits"""
-    return random.randint(0, 2**length - 1)
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def count_ones(genes : int) -> int:
-    """ Convert to binary representation and count the 1's"""
-    return bin(genes).count("1")
+def random_sequence(n):
+    """
+    Generate random bitstring of length n
+    :param n:
+    :return:
+    """
+    bitlist = np.random.randint(2, size=n)
+    return ' '.join(str(bit) for bit in bitlist)
 
-def mc(fitness_function : Callable[[int], int], n : int, iterations : int) -> (int, int):
 
+def count_ones(genes):
+    """
+    Summation of bitstring, implement as number of 1s in string
+    :param genes: bitstring
+    :return:
+    """
+    return genes.count("1")
+
+
+def mc(fitness_function, n, iterations):
+    best_fitness_list = []
     best_individual = random_sequence(n)
     best_fitness = fitness_function(best_individual)
 
     for i in range(iterations):
+        best_fitness_list.append(best_fitness)
         sequence = random_sequence(n)
         fitness = fitness_function(sequence)
         if fitness > best_fitness:
             best_individual = sequence
             best_fitness = fitness
-        
-    return best_individual, best_fitness
+
+    return best_individual, best_fitness, best_fitness_list
+
 
 if __name__ == "__main__":
-    print(mc(count_ones, 10, 1500))
-    print("running the algorithm 1500 * 10 times")
-    print("best individual fount: 0 times")
+    n_iter = 1500
+    n = 100
+    print("Running the algorithm 10 times with: \n \t n_iters: {} \n \t n: {}".format(n_iter, n))
+    times_found = 0
+    fitnesses = []
+    for _ in range(10):
+        best_individual, best_fitness, best_fitness_list = mc(count_ones, n, n_iter)
+        times_found += 1 if best_fitness is n else 0
+        fitnesses.append(best_fitness_list)
 
-#  the chance to not find the best individual in 1500 runs is:
-# 0.999999999999999999999999998816708642168482291882407152775...
+    for fitness in fitnesses:
+        plt.plot(fitness)
+    plt.xlabel("Iterations")
+    plt.ylabel("Best Fitness")
+    plt.show()
 
-# print(fitness(2**1-1))
+    print("Best fitness is found {} times".format(times_found))
